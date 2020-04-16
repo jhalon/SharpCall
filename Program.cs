000f43f2e8f38f1fa550716fb157bcfcee28b59d
Jack Halon <jacek.halon@gmail.com>
2020-04-16 16:36:08 -0500

    Upload PoC

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//
+// Author: Jack Halon (@jack_halon)
+// Project: SharpCall (https://github.com/jhalon/SharpCall)
+// License: BSD 3-Clause
+//
+
+using System;
+using System.Runtime.InteropServices;
+
+using static SharpCall.Native;
+using static SharpCall.Syscalls;
+
+namespace SharpCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Win32.SafeHandles.SafeFileHandle fileHandle;
+            UNICODE_STRING filename = new UNICODE_STRING();
+            RtlInitUnicodeString(ref filename, @"\??\C:\Users\User\Desktop\test.txt");
+            IntPtr objectName = Marshal.AllocHGlobal(Marshal.SizeOf(filename));
+            Marshal.StructureToPtr(filename, objectName, true);
+
+            OBJECT_ATTRIBUTES FileObjectAttributes = new OBJECT_ATTRIBUTES
+            {
+                Length = (int)Marshal.SizeOf(typeof(OBJECT_ATTRIBUTES)),
+                RootDirectory = IntPtr.Zero,
+                ObjectName = objectName,
+                Attributes = 0x00000040, // OBJ_CASE_INSENSITIVE
+                SecurityDescriptor = IntPtr.Zero,
+                SecurityQualityOfService = IntPtr.Zero
+            };
+
+            IO_STATUS_BLOCK IoStatusBlock = new IO_STATUS_BLOCK();
+            long allocationSize = 0;
+
+            var status = NTCreateFile(
+                out fileHandle,
+                FileAccess.FILE_GENERIC_WRITE,
+                ref FileObjectAttributes,
+                ref IoStatusBlock,
+                ref allocationSize,
+                FileAttributes.Normal,
+                FileShare.Write,
+                CreationDisposition.FILE_OVERWRITE_IF,
+                CreateOption.FILE_SYNCHRONOUS_IO_NONALERT,
+                IntPtr.Zero,
+                0);
+        }
+    }
+}
---
+++ Syscalls.cs
@@ -0,0 +1,95 @@
+﻿//
+// Author: Jack Halon (@jack_halon)
+// Project: SharpCall (https://github.com/jhalon/SharpCall)
+// License: BSD 3-Clause
+//
+
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+using static SharpCall.Native;
+
+namespace SharpCall
+{
+    class Syscalls
+    {
+
+        static byte[] bNtCreateFile =
+        {
+            0x4C, 0x8B, 0xD1,               // mov r10, rcx
+            0xB8, 0x55, 0x00, 0x00, 0x00,   // mov eax, 0x55 (NtCreateFile Syscall)
+            0x0F, 0x05,                     // syscall
+            0xC3                            // ret
+        };
+
+        public static NTSTATUS NTCreateFile(
+            out Microsoft.Win32.SafeHandles.SafeFileHandle FileHandle,
+            FileAccess DesiredAcces,
+            ref OBJECT_ATTRIBUTES ObjectAttributes,
+            ref IO_STATUS_BLOCK IoStatusBlock,
+            ref long AllocationSize,
+            FileAttributes FileAttributes,
+            FileShare ShareAccess,
+            CreationDisposition CreateDisposition,
+            CreateOption CreateOptions,
+            IntPtr EaBuffer,
+            uint EaLength)
+        {
+            byte[] syscall = bNtCreateFile;
+
+            unsafe
+            {
+                fixed (byte* ptr = syscall)
+                {
+                    IntPtr memoryAddress = (IntPtr)ptr;
+
+                    if (!VirtualProtect(memoryAddress, (UIntPtr)syscall.Length, (uint)AllocationProtect.PAGE_EXECUTE_READWRITE, out uint lpflOldProtect))
+                    {
+                        throw new Win32Exception();
+                    }
+
+                    Delegates.NtCreateFile assembledFunction = (Delegates.NtCreateFile)Marshal.GetDelegateForFunctionPointer(memoryAddress, typeof(Delegates.NtCreateFile));
+
+                    return (NTSTATUS)assembledFunction(out FileHandle,
+                        DesiredAcces,
+                        ref ObjectAttributes,
+                        ref IoStatusBlock,
+                        ref AllocationSize,
+                        FileAttributes,
+                        ShareAccess,
+                        CreateDisposition,
+                        CreateOptions,
+                        EaBuffer,
+                        EaLength);
+                }
+            }
+        }
+
+        public struct Delegates
+        {
+            [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+            public delegate NTSTATUS NtCreateFile(
+                out Microsoft.Win32.SafeHandles.SafeFileHandle FileHandle,
+                FileAccess DesiredAcces,
+                ref OBJECT_ATTRIBUTES ObjectAttributes,
+                ref IO_STATUS_BLOCK IoStatusBlock,
+                ref long AllocationSize,
+                FileAttributes FileAttributes,
+                FileShare ShareAccess,
+                CreationDisposition CreateDisposition,
+                CreateOption CreateOptions,
+                IntPtr EaBuffer,
+                uint EaLength
+                );
+        }
+    }
+}
+
+
+
+
+
+
+
+
